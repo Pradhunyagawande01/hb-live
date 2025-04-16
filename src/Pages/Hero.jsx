a25e96f910fa3f071c8067e19cf9bc0f5c746f33
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2025-04-16 23:42:48 +0530

    new changes

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -278,7 +278,7 @@ const Hero = () => {
 
                       {/* Content */}
                       <span className="relative z-20 uppercase tracking-wider flex items-center gap-2">
-                        Call for Voleenteers
+                        Call for Vvlunteers
                       </span>
 
                       <BiSolidChevronDown className="text-xl ml-2 relative z-20 animate-bounce" />
