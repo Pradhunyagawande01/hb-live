aeeaae6ef9a956a350e98597b020eb4b3b0524c1
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2026-06-22 14:11:17 +0530

    chore: remove unused LinkedIn import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React from "react";
+
 import Landing from "./components/Landing";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Partners from "./components/Partners";
-// import Linkedin from "./components/Linkedin";
 import Botum from "./components/Botum";
 import Footer from "./components/Footer";
 import LinkedInWallSection from "./components/LinkedInWall";
